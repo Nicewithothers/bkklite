7dfa6885ade71a808d653d6772b4b3cb289abc40
Bondár Kristóf <brkristof5@gmail.com>
2023-02-13 19:25:59 +0100

    Képek feltöltése, routing alkalamzás

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexModule } from "@angular/flex-layout";
+import {MainModule} from "./pages/main/main.module";
+import {CommonModule} from "@angular/common";
+import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
   declarations: [
@@ -10,9 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexModule,
+    MainModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MainComponent} from "./main.component";
 import {MainRoutingModule} from "./main-routing.module";
+import {MatCardModule} from "@angular/material/card";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import {FlexModule} from "@angular/flex-layout";
 
 
 
@@ -11,7 +15,11 @@ import {MainRoutingModule} from "./main-routing.module";
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    FlexModule
   ]
 })
 export class MainModule { }
